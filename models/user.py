66cd05df9221ffa0efabba87ddcddbbeda6f1813
Tina Yang <Tinayangers@gmail.com>
2023-03-24 18:08:19 -0400

    Added __init__ method to user model

--- models/user.py
+++ models/user.py
@@ -8,9 +8,16 @@ class User(db.Model):
     name = db.Column(db.String(80))
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
+    image = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=str(
         datetime.utcnow()), nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow(
     ), nullable=False, onupdate=datetime.now())
     comments = db.relationship("Comment", cascade="all", back_populates="user")
     user_manga_lists = db.relationship("User_Manga_List", cascade="all", back_populates="user")
+
+    def __init__(self, name, email, password, image):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.image = image
