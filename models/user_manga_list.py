479d7d3767399c12e319426b4a53df8e8cd1c45b
Tina Yang <Tinayangers@gmail.com>
2023-03-24 19:06:40 -0400

    Added init method to user manga list model

--- models/user_manga_list.py
+++ models/user_manga_list.py
@@ -9,8 +9,14 @@ class User_Manga_List(db.model):
     manga_id=db.Column(db.Integer
                     #    , db.ForeignKey('manga.id', nullable=False)
                     )
+    favorite_list=db.Column(db.Array)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow(
     ), nullable=False, onupdate=datetime.utcnow)
     user = db.relationship("User", back_populates="user_manga_lists")
     # manga = db.relationship("Manga", back_populates="user_manga_lists")
+
+    def __init__(self, user_id, manga_id, favorite_list):
+        self.user_id = user_id
+        self.manga_id = manga_id
+        self.favorite_list = favorite_list
