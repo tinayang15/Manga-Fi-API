41ef33102434b3c153ac1b0fa84da550edb1ea5a
Tina Yang <Tinayangers@gmail.com>
2023-03-24 18:04:36 -0400

    Added table columns to user model

--- models/user.py
+++ models/user.py
@@ -3,3 +3,13 @@ from models.db import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=str(
+        datetime.utcnow()), nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow(
+    ), nullable=False, onupdate=datetime.now())
+    doctors = db.relationship("Doctor", cascade="all", back_populates="patient")
