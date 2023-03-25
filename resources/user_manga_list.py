2deca320f190ec0a157b3a8068fd58b2b693102c
Tina Yang <Tinayangers@gmail.com>
2023-03-25 12:07:46 -0400

    Added post method to user_manga_list class

--- resources/user_manga_list.py
+++ resources/user_manga_list.py
@@ -7,3 +7,12 @@ class User_Manga_Lists(Resource):
     def get(self):
         user_manga_lists = User_Manga_List.find_all()
         return user_manga_lists
+    
+    def post(self):
+        data = request.get_json()
+        params = {}
+        for k in data.keys():
+            params[k]=data[k]
+        user_manga_list = User_Manga_List(**params)
+        user_manga_list.create()
+        return user_manga_list.json(), 201
