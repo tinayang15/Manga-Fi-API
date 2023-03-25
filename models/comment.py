561b31a929aeceae311e9f0c7f835edc24082829
Tina Yang <Tinayangers@gmail.com>
2023-03-24 23:43:39 -0400

    Add table columns for comment model

---
+++ models/comment.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from models.db import db
+from flask import request
+
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
+    manga_id=db.Column(db.Integer
+                    #    , db.ForeignKey('manga.id', nullable=False)
+                    )
+    content=db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow(
+    ), nullable=False, onupdate=datetime.utcnow)
+    user = db.relationship("User", back_populates="comments")
