e92194deef0a5564bed39b39a2dec272db4a9163
Tina Yang <Tinayangers@gmail.com>
2023-03-25 11:28:20 -0400

    Added class Users to user resource

---
+++ resources/user.py
@@ -0,0 +1,10 @@
+from flask import request
+from flask_restful import Resource
+from models.user import User
+from models.db import db
+from sqlalchemy.orm import joinedload
+
+class Users(Resource):
+    def get(self):
+        users = User.find_all()
+        return [u.json() for u in users]
